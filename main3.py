7f1706dbcd29e1a27500c0a6d8cda39fcb580aca
JiYiqaq <macdonaldchloe794@gmail.com>
2023-04-06 12:57:46 +0800

    求100-999之间的水仙花数

--- main3.py
+++ main3.py
@@ -1,4 +1,7 @@
-for a in range(1,10):
-    for b in range(1,a+1):
-        print("{}*{}={}".format(a,b,a*b),end=" ")
-    print( )
+for i in range(100,999):
+        a=int(i/100)
+        b=int(i%100/10)
+        c=int(i%10)
+        if i==(a**3+b**3+c**3):
+                print(str(i)+"是水仙花数")
+
