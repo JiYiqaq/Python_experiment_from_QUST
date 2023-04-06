a39db2e338458e8867409ac9a0927ff2d2235eb3
JiYiqaq <macdonaldchloe794@gmail.com>
2023-04-06 12:57:55 +0800

    输入一个数，判断是否为回文数

--- main4.py
+++ main4.py
@@ -1,4 +1,10 @@
-a=str(input("请输入一个字符串:"))
-b=a[::-1]
-print(a)
-print(b)
+a=int(input("请输入一个数:"))
+num1=a
+num=0
+while num1>0:
+    num=num*10+num1%10
+    num1//=10
+if a==num:
+    print("{}是回文数".format(a))
+else:
+    print("{}不是回文数".format(a))
